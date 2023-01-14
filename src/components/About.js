5ab67a9595862421c0415dd572d8f78ad4c2a4d6
Kavita Jakhar <Kavya21@gmail.com>
2023-01-14 16:05:49 +0530

    reset the about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,20 +4,16 @@ import React from 'react'
 const About = () => {
   return (
     <>
-      <div class="container">
-
-        <div class="col-md-12">
-          <h2 class="text-center my-3">About Us</h2>
+      <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+        <div className="flex flex-col lg:flex-row justify-between gap-8">
+          <div className="w-full lg:w-5/12 flex  my-3 flex-col justify-center">
+            <h2 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h2>
+            <p className="font-normal text-base leading-6 text-gray-600 ">A short note book app  allows users to quickly and easily create, organize, and access notes on their smartphones or tablets and desktops. These app typically feature a simple, user-friendly interface that makes it easy to create new notes, add text. this short note book app also offer the ability to login your account with your password across multiple devices, making it easy to access important information no matter where you are. </p>
+          </div>
+          <div className="w-full lg:w-8/12 ">
+            <img className="w-50" src="https://images.unsplash.com/photo-1505744386214-51dba16a26fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1906&q=80" alt="A group of People" />
+          </div>
         </div>
-      </div>
-      <div class="d-flex text-center">
-        <div class="col-md-6 my-3 border border-dark  rounded mx-3">
-          <p>A note-saving application is a tool that allows users to create, edit, and organize notes on their electronic devices. These notes can include text. Some popular note-saving apps include Evernote, OneNote, and Google Keep. These apps often feature a variety of organizational tools, such as the ability to create notebooks, tag notes, and search for specific notes. Additionally, many note-saving apps offer the ability to sync notes across multiple devices, so users can access their notes on their phone, tablet, and computer. Some apps also offer the ability to collaborate with others on shared notes. Overall, note-saving apps are a convenient and efficient way to keep track of important information and ideas.</p>
-        </div>
-        <div class="col-md-6 my-3  border border-dark rounded mx-3">
-          <p>There are many different note-saving apps available on the market, each with their own unique features and capabilities. Some popular note-saving apps include Evernote, OneNote, and Google Keep. These apps often feature a variety of organizational tools, such as the ability to create notebooks, tag notes, and search for specific notes. Additionally, many note-saving apps offer the ability to sync notes across multiple devices, so users can access their notes on their phone, tablet, and computer. This allows users to easily access their notes from anywhere, at any time.</p>
-        </div>
-
       </div>
 
     </>
